a54f2e51797320ac8fa32467d1587bb8b11fd2ae
veerendra789 <154229971+veerendra789@users.noreply.github.com>
2025-12-31 14:55:06 +0530

    Time: 2113 ms (16.69%), Space: 41.8 MB (92.86%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+       int s=0;
+       for(int i=1;i<=num/2;i++){
+        if(num%i==0){
+            s=s+i;
+        }
+       } 
+       if(s==num){
+        return true;
+       }
+    return false;
+    }
+}
