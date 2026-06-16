c76ede0236e213bad9952ea65458c495b0f5d35f
Nellipudi Satya Veerendra <154229971+veerendra789@users.noreply.github.com>
2026-06-16 10:48:11 +0530

    Time: 0 ms (100%), Space: 39.9 MB (42.05%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     string processStr(string s) {
-        int n=s.size();
-        string result;
-        for(int i=0;i<n;i++){
-            if(s[i]=='*'){
-                result=ass(result);
-            }
-            else if(s[i]=='#'){
-                result=hash(result);
-            }
-            else if(s[i]=='%'){
-                result=mod(result);
-            }
-            else{
-                result+=s[i];
-            }
+        int n = s.size();
+        string ans = "";
+        for(char c : s){
+         if(c >= 'a' && c <= 'z'){
+            ans.push_back(c);
+         }
+
+         else if(c == '*'){
+            if(!ans.empty()) ans.pop_back();
+         }
+         else if(c=='#'){
+            ans+=ans;
+         }else if(c == '%'){
+            reverse(ans.begin(),ans.end());
+         }
         }
-        return result;
-    }
-    string ass(string s){
-        if(s.empty()) return "";
-        string res;
-        for(int i=0;i<s.size()-1;i++){
-            res+=s[i];
-        }
-        return res;
-    }
-    string hash(string s){
-        string res=s;
-        for(int i=0;i<s.size();i++){
-            res+=s[i];
-        }
-        return res;
-    }
-    string mod(string s){
-        string rev(s.rbegin(), s.rend());
-        return rev;
+        return ans;
+
+
     }
 };
