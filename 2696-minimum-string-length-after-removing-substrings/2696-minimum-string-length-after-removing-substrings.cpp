68cedcf53f18f1459eab40182029fb81c6882676
Nellipudi Satya Veerendra <154229971+veerendra789@users.noreply.github.com>
2026-06-17 14:25:23 +0530

    Time: 4 ms (32.17%), Space: 11.8 MB (23.25%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(char c:s){
+            if((!st.empty())&&((c=='B' && st.top()=='A') ||(c=='D' &&st.top()=='C'))){
+                if(!st.empty()) st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        return st.size();
+    }
+};
